c2bae76f8be173e3ddf541df38a46b2528309a67
GJMcGowan <gemcgow@gmail.com>
2015-06-11 15:27:27 +0100

    adding images

--- public/app.js
+++ public/app.js
@@ -10,6 +10,6 @@ app.config(function($routeProvider) {
 app.controller('MainController', function($scope) {
   $scope.projects = [
     {"id": 1, "title": "Virgin Galactic Launch", "text": "Airline offering suborbital spaceflights with a new version of SpaceShipOne. Provides description of flights, the vehicle and offers bookings starting 2005.", "images": ["/public/images/virgin.jpg"] },
-    {"id": 2, "title": "Tesco", "text": "Tesco PLC is a British multinational grocery and general merchandise retailer headquartered in Cheshunt, Hertfordshire, England, United Kingdom. It is the third large…", "images": ["/public/images/tesco.jpg"]},
+    {"id": 2, "title": "Tesco", "text": "Tesco PLC is a British multinational grocery and general merchandise retailer headquartered in Cheshunt, Hertfordshire, England, United Kingdom.[3] It is the third largest retailer in the world measured by profits[4][5] and second-largest retailer in the world measured by revenues. It has stores in 12 countries across Asia and Europe and is the grocery market leader in the UK (where it has a market share of around 28.4%), Ireland, Hungary,[6] Malaysia, and Thailand.[7][8]", "images": ["/public/images/tesco.jpg"]},
   ];
 });
