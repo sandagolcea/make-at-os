d05c429504614baec09e8b17a1bce33565908927
GJMcGowan <gemcgow@gmail.com>
2015-06-11 16:01:33 +0100

    Cleaning up and styling

--- public/app.js
+++ public/app.js
@@ -24,5 +24,8 @@ app.controller('MainController', function($scope) {
     {"id": 1, "title": "Virgin Galactic Launch", "tag": "entertainment", "text": "Airline offering suborbital spaceflights with a new version of SpaceShipOne. Provides description of flights, the vehicle and offers bookings starting 2005.", "images": ["/public/images/virgin.jpg"] },
     {"id": 2, "title": "Tesco", "tag": "corporate", "text": "Tesco PLC is a British multinational grocery and general merchandise retailer headquartered in Cheshunt, Hertfordshire, England, United Kingdom. It is the third large…", "images": ["/public/images/tesco.jpg"]},
     {"id": 3, "title": "Sony", "tag": "media", "text": "Sony is Sanda's favourite brand", "images": ["/public/images/tesco.jpg"]},
+    {"id": 4, "title": "Virgin Galactic Launch", "tag": "entertainment", "text": "Airline offering suborbital spaceflights with a new version of SpaceShipOne. Provides description of flights, the vehicle and offers bookings starting 2005.", "images": ["/public/images/virgin.jpg"] },
+    {"id": 5, "title": "Tesco", "tag": "corporate", "text": "Tesco PLC is a British multinational grocery and general merchandise retailer headquartered in Cheshunt, Hertfordshire, England, United Kingdom. It is the third large…", "images": ["/public/images/tesco.jpg"]},
+    {"id": 6, "title": "Sony", "tag": "media", "text": "Sony is Sanda's favourite brand", "images": ["/public/images/tesco.jpg"]},
   ];
 });
